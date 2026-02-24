0eda0c0fcadf43c5095cf30e534704b968e0c1cb
thiagoraquel <thiaguinho2005@yahoo.com.br>
2026-02-24 18:34:59 -0300

    funções admin

--- commerce/commerce/auctions/admin.py
+++ commerce/commerce/auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, AuctionListing, Bid, Comment
 
 # Register your models here.
+# Registrando os modelos para que apareçam na interface do Admin
+admin.site.register(User)
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
